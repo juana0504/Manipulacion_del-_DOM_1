e00f41d2706731667b4b5b3ea019e19bdd665f30
Juana Rodriguez <rodriguezjuana212@gmail.com>
2025-09-02 14:37:17 -0500

    Agregue manipulacion de atributos en JS

---
+++ js/manipular-atributos.js
@@ -0,0 +1,12 @@
+const btnfacebook=document.getElementById('btn-facebook');
+
+const imagen=document.querySelector('#imagen-unica');
+
+const face=document.getElementById('btn-facebook');
+
+
+btnfacebook.innerHTML='<img src="img/facebook.png" alt="facebook">';
+
+imagen.setAttribute("alt","Texto alternativo agregado por JavaScript");
+
+face.setAttribute("href","https://www.facebook.com/");
